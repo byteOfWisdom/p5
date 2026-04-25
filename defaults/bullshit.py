e13048d0e169569638fc6a73909e0fe4c5b53b4b
Leoni <leoni.dessau@mailbox.org>
2026-04-25 12:36:37 +0200

    initial commit

---
+++ defaults/bullshit.py
@@ -0,0 +1,12 @@
+import locale
+import matplotlib.pyplot as plt
+
+
+def this_is_fucking_stupid_no_one_actually_gives_a_fuck():
+    # Set to German locale to get comma decimal separater
+    locale.setlocale(locale.LC_NUMERIC, 'de_DE.UTF-8')
+
+    plt.rcdefaults()
+
+    # Tell matplotlib to use the locale we set above
+    plt.rcParams['axes.formatter.use_locale'] = True
