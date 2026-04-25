fecce01ec27be47c04b6f010ba647e922e1c812c
Leoni <leoni.dessau@mailbox.org>
2026-04-25 13:04:47 +0200

    adsf

--- 521-gamma-spektroskopie/scripts/spectrum.py
+++ 521-gamma-spektroskopie/scripts/spectrum.py
@@ -1,13 +1,11 @@
 from sys import argv
 from matplotlib import pyplot as plt
 import numpy as np
-
+import spectrum_fit
 
 def main():
     data = np.transpose(np.loadtxt(argv[1]))
-    plt.plot(data[0], data[1])
-    plt.show()
-
+    spectrum_fit.decomp_spectrum(data[0], data[1])
 
 if __name__ == "__main__":
     main()
