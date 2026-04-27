a427ee116b1c406418f856d08e3175b1457b0392
Leoni <leoni.dessau@mailbox.org>
2026-04-27 19:40:19 +0200

    pseudo-structuring the code

--- 521-gamma-spektroskopie/scripts/spectrum_fit.py
+++ 521-gamma-spektroskopie/scripts/spectrum_fit.py
@@ -26,43 +26,74 @@ def fit_biggest_peak(channel, amplitude):
     p0 = [(channel[upper] - channel[lower]) / 2.5]
     channel_cut = channel[lower:upper]
     amplitude_cut = amplitude[lower:upper]
-    fit_res, (_, goodness) = std.fit_func(lambda x, sigma: std.gaussian(x, amplitude[max_id], channel[max_id], sigma), channel_cut, amplitude_cut, p0=p0, force_cf=True)
+    fit_res, (_, goodness) = std.fit_func(
+        lambda x, sigma: std.gaussian(x, amplitude[max_id], channel[max_id], sigma),
+        channel_cut, amplitude_cut, p0=p0, force_cf=True)
     if plot_subfits:
         plt.plot(channel_cut, amplitude_cut, color="red")
     snr = np.log(amplitude[max_id]) * (upper - lower)
     return np.array([amplitude[max_id], channel[max_id], fit_res[0]]), snr
 
 
-def decomp_spectrum(channel, amplitude, underground_fn, ug_arg_count, save_fig=False):
+def strip_spectrum(channel, amplitude, ref_level):
     fits = []
     snr = []
 
-    ug_fit, _ = std.fit_func(underground_fn, channel[amplitude < 0.05 * max(amplitude)], amplitude[amplitude < 0.05 * max(amplitude)], force_cf=True)
-    
-    reduced_amps = copy.copy(amplitude)
-    reduced_amps = reduced_amps - underground_fn(channel, *ug_fit)
-    while max(reduced_amps) > 0.1 * max(amplitude):
+    while max(amplitude) > ref_level:
         if plot_subfits:
-            plt.plot(channel, reduced_amps)
-        res, r_sq = fit_biggest_peak(channel, reduced_amps)
+            plt.plot(channel, amplitude)
+        res, r_sq = fit_biggest_peak(channel, amplitude)
         if plot_subfits:
             print(r_sq)
             plt.plot(channel, std.gaussian(channel, *res), linestyle="dashed")
             plt.show()
 
-        reduced_amps -= std.gaussian(channel, *res)
-        reduced_amps[reduced_amps < 0] = 0
+        amplitude -= std.gaussian(channel, *res)
+        amplitude[amplitude < 0] = 0
         fits.append(np.abs(res))
         snr.append(r_sq)
+    return fits, snr
 
+
+def make_spectrum_function(linecount, underground_fn):
+    all_lines = std.make_n_gaussian(linecount)
+    return lambda x, *args: all_lines(x, *args[:3 * linecount]) + underground_fn(x, *args[3 * linecount:])
+    
+
+def plot_results(channel, amplitude, total_spectrum, underground_fn, res, ug_arg_count,  save_fig, goodness, lc):
+    std.default.plt_pretty("Kanal", "Anzahl")
+    plt.plot(channel, amplitude)
+    plt.plot(channel, total_spectrum(channel, *res), label=f"$R^2={round(goodness, 3)}$")
+    plt.legend()
+    for i in range(lc):
+        plt.plot(
+            channel,
+            std.gaussian(channel, res[3 * i], res[3 * i + 1], res[3 * i + 2]) + underground_fn(channel, *res[-ug_arg_count:]),
+            linestyle="dashed")
+    plt.vlines(res[1:-ug_arg_count:3], 0, 500, colors="green", linestyles="dotted", linewidth=1)
+    if save_fig:
+        plt.savefig(save_fig)
+        plt.cla()
+    else:
+        plt.show()
+
+def decomp_spectrum(channel, amplitude, underground_fn, ug_arg_count, save_fig=False):
+    fits = []
+    snr = []
+
+    ug_fit, _ = std.fit_func(
+        underground_fn, channel[amplitude < 0.05 * max(amplitude)],
+        amplitude[amplitude < 0.05 * max(amplitude)], force_cf=True)
+    
+    reduced_amps = copy.copy(amplitude)
+    reduced_amps = reduced_amps - underground_fn(channel, *ug_fit)
+    fits, snr = strip_spectrum(channel, reduced_amps, 0.1 * max(amplitude))
 
     # filter sensible peaks
     valid_lines = []
     lc = 0
 
-    # print("a    \t   mu  \t     sigma")
     for params, snr in zip(fits, snr):
-        # print(f"{params[0]}\t{params[1]}\t{params[2]}\t")
         if snr < 0.1:
             print("this should not be printed!!")
             continue
@@ -77,22 +108,12 @@ def decomp_spectrum(channel, amplitude, underground_fn, ug_arg_count, save_fig=F
 
     if plot_subfits:
         print(lc)
-    all_lines = std.make_n_gaussian(lc)
-    with_ug = lambda x, *args: all_lines(x, *args[:3 * lc]) + underground_fn(x, *args[3 * lc:])
-    res, (std_devs, goodness) = std.fit_func(with_ug, channel, amplitude, y_errors=np.sqrt(amplitude), p0=np.append(valid_lines, np.zeros(ug_arg_count)), force_cf=True)
+    total_spectrum = make_spectrum_function(lc, underground_fn)
+    res, (std_devs, goodness) = std.fit_func(
+        total_spectrum, channel, amplitude, y_errors=np.sqrt(amplitude),
+        p0=np.append(valid_lines, np.zeros(ug_arg_count)), force_cf=True)
 
-    std.default.plt_pretty("Kanal", "Anzahl")
-    plt.plot(channel, amplitude)
-    plt.plot(channel, with_ug(channel, *res), label=f"$R^2={round(goodness, 3)}$")
-    plt.legend()
-    for i in range(lc):
-        plt.plot(channel, std.gaussian(channel, res[3 * i], res[3 * i + 1], res[3 * i + 2]) + underground_fn(channel, *res[-ug_arg_count:]), linestyle="dashed")
-    plt.vlines(res[1:-ug_arg_count:3], 0, 500, colors="green", linestyles="dotted", linewidth=1)
-    if save_fig:
-        plt.savefig(save_fig)
-        plt.cla()
-    else:
-        plt.show()
+    plot_results(channel, amplitude, total_spectrum, underground_fn, res, ug_arg_count, save_fig, goodness, lc)
 
     res = p.ev(res, std_devs)
     res_dict = {
