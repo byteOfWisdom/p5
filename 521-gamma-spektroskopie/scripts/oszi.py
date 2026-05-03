db3f8fd7212f38cadf282bdaa3c7ca20078472ff
Lena <lena@email.com>
2026-05-03 18:40:06 +0200

    exponential fitting

--- 521-gamma-spektroskopie/scripts/oszi.py
+++ 521-gamma-spektroskopie/scripts/oszi.py
@@ -1,6 +1,6 @@
 from matplotlib import pyplot as plt
 import numpy as np
-from numpy._core import half
+from scipy.optimize import curve_fit
 import std
 from sys import argv
 import propeller as p
@@ -12,9 +12,10 @@ def get_data(file):
 def rel_volts(volts):
     min_volt = min(volts)
     #print(min_volt)
-    corr_volts = []
-    for i in range(len(volts)):
-        corr_volts.append(volts[i]+abs(min_volt))
+    # corr_volts = []
+    # for i in range(len(volts)):
+    #     corr_volts.append(volts[i]+abs(min_volt))
+    corr_volts = volts-min(volts)
     #print(corr_volts[50])
     return corr_volts
 
@@ -30,7 +31,6 @@ def plot_as_seperate(times, voltages,file,index,corrected=False):
     plt.show()
     plt.close()
     return
-
 
 def plot_as_subfigs(files,corrected=False):
     fig, axs = plt.subplots(nrows=2, ncols=2)
@@ -73,7 +73,7 @@ def plotting_figs(files,seperate=True):
 
 def analyse_shape(file):
     times, volts, _ = get_data(file)
-    print("file ",file,":")
+    #print("file ",file,":")
     #plot_as_seperate(times, volts, argv[1], argv[1])
 
     times_div = 0.02e-6
@@ -83,33 +83,60 @@ def analyse_shape(file):
     max_volt = max(volts_corr)
     max_time_index = np.where(volts_corr == max_volt)
     max_time = times_w_err[max_time_index]
-    print("maximum at:", max_time[0].format(), " index: ", max_time_index[0][0],"\n with: ", max_volt.format(),"V")
+    #print("maximum at:", max_time[0].format(), " index: ", max_time_index[0][0],"\n with: ", max_volt.format(),"V")
 
     sliced_volts = volts_corr[max_time_index[0][0]:]
-    #print(sliced_volts)
     half_volt = max_volt/np.exp(1)
-    #print(~half_volt)
     half_rel_index = np.where(~sliced_volts < ~half_volt)[0][0]
     half_index = max_time_index+half_rel_index
     half_volt = volts_corr[half_index]
     half_time = times_w_err[half_index]
-    print("half height of ", half_volt[0][0].format(), "V at ", half_time[0][0].format(), "s" )
+    #print("half height of ", half_volt[0][0].format(), "V at ", half_time[0][0].format(), "s" )
 
     duration = half_time - max_time
-    print("duration: ", duration[0][0].format(), "s")
-    return max_volt, duration[0][0]
+    #print("duration: ", duration[0][0].format(), "s")
+    return max_volt, duration[0][0], max_time_index[0][0],half_index[0][0]
 
 def av_shape(files):
     max_volts = []
     durations = []
     for i in files:
-        max_volt, duration = analyse_shape(i)
+        max_volt, duration,_,_ = analyse_shape(i)
         max_volts.append(max_volt)
         durations.append(duration)
     av_max = sum(max_volts)/len(max_volts)
     av_duration = sum(durations)/len(durations)
     print("average height: ",av_max.format(), "V\naverage duration: ",av_duration.format(),"s")
     return
+
+def exponential(x,a,b,c, t0):
+    return c*np.exp(a*(x - t0))+b
+
+
+def fit_shape(file):
+    times, volts, _ = get_data(file)
+    max_volt,duration, max_time_index, half_index = analyse_shape(file)
+    init_guess = [-1e4,0.6,0.08,times[max_time_index]]
+    upper_lim = int(half_index + (half_index - max_time_index))
+    lower_lim = int(max_time_index-(max_time_index/8))
+
+    #fit data to exponential
+    fit, (sd, rsq) = std.fit_func(exponential, times[max_time_index:upper_lim], volts[max_time_index:upper_lim], p0=init_guess)
+    params = p.ev(fit,sd)
+
+    #data viz
+    print("fit (c*np.exp(a*(x - d))+b):")
+    for i in range(len(params)):
+        print(params[i].format())
+    print("R^2:",round(rsq,3))
+    plt.plot(times[lower_lim:upper_lim+1300], volts[lower_lim:upper_lim+1300], label="gekürzte Messdaten (NaI-Detektor)",color="orange")
+    plt.plot(times[max_time_index:upper_lim], exponential(times[max_time_index:upper_lim], *fit),label=f"Anpassungsfunktion, $R^2$={round(rsq,3)}",color="darkblue")
+    plt.legend(loc="best",fontsize="small")
+    std.default.plt_pretty(r"Zeit / s","Spannung / V")
+    #plt.show()
+    plt.savefig("../figs/peak_curve_HPGe_"+file+".pdf")
+    return
+
 
 
 def main():
@@ -119,7 +146,8 @@ def main():
         # 1 3 4 5
     # for i in range(1,len(argv)):
     #     analyse_shape(argv[i])
-    av_shape(argv[1:])
+    #av_shape(argv[1:])
+    fit_shape(argv[1])
 
     return
 
