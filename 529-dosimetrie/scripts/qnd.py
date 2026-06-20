d46797abd818985d85d5f8d8f9c3bf6b1c594d78
Leoni <leoni.dessau@mailbox.org>
2026-06-20 16:33:37 +0200

    default errorbar shorthand

--- 529-dosimetrie/scripts/qnd.py
+++ 529-dosimetrie/scripts/qnd.py
@@ -1,14 +1,21 @@
 import std
-from matplotlib import pyplot as plt
 import tomllib
-from sys import argv
 
-fhandle = open("../data/plate_cap.toml", "rb")
-metadata = tomllib.load(fhandle)
+fhandle = open("../data/meta.toml", "rb")
+metadata = tomllib.load(fhandle)["dosimetrie"]
 fhandle.close()
 
 
+def plot_measurement(measurement):
+    print(metadata[measurement])
+    data = std.util.load_csv("../data/" + metadata[measurement]["csv_file"], skiprows=1)
+    print(data[0])
+    print(data[1])
+    std.default.plt_errorbar(data[0], data[1])
 
-data = std.util.load_csv("../data/" + metadata["dosimetrie"][argv[1]]["csv_file"], skiprows=1)
-plt.scatter(data[0], data[1])
-plt.show()
+
+plot_measurement("messung_1")
+plot_measurement("messung_2")
+plot_measurement("messung_3")
+plot_measurement("messung_4")
+std.default.plt_finish("x", "y")
