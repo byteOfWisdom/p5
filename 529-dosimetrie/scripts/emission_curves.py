8a925f41672ab8ef969f6f7276e82798d15aaa63
Leoni <leoni.dessau@mailbox.org>
2026-06-20 17:19:25 +0200

    asdf

--- 529-dosimetrie/scripts/emission_curves.py
+++ 529-dosimetrie/scripts/emission_curves.py
@@ -4,19 +4,26 @@ import tomllib
 import propeller as p
 
 fhandle = open("../data/meta.toml", "rb")
-metadata = tomllib.load(fhandle)["dosimetrie"]
+metadata = tomllib.load(fhandle)["emission"]
 fhandle.close()
 
 
 def plot_measurement(measurement):
     print(metadata[measurement])
     data = std.util.load_csv("../data/" + metadata[measurement]["csv_file"], skiprows=1)
-    cap_voltage, amp_voltage = data[0], data[1]
+    x_var, amp_voltage = data[0], data[1]
     resistance = p.ev(metadata[measurement]["resistor"], 2e-2 * metadata[measurement]["resistor"])
     ion_current = amp_voltage / resistance
-    label = f"$U_B$ = {metadata[measurement]["acceleration_voltage"] * 1e-3} kV" 
-    std.default.plt_errorbar(cap_voltage, ion_current, label)
+    label = metadata[measurement]["element"] 
+    std.default.plt_errorbar(x_var, ion_current, label)
 
 
 print(metadata)
-# std.default.plt_finish("Kondensatorspannung $U_C$ / V", "Ionisationsstrom $I_C$ / A")
+plot_measurement("current_1")
+plot_measurement("current_2")
+std.default.plt_finish("Heizstrom $I_H$ / A", "Ionisationsstrom $I_C$ / A")
+
+
+plot_measurement("voltage_1")
+plot_measurement("voltage_2")
+std.default.plt_finish("Beschleunigungsspannung $U_B$ / V", "Ionisationsstrom $I_C$ / A")
