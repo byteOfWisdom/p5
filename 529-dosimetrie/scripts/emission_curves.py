ae92cd0703f55473c844d8c0f3feb78b1e3f31b5
Leoni <leoni.dessau@mailbox.org>
2026-06-20 17:36:27 +0200

    adf

--- 529-dosimetrie/scripts/emission_curves.py
+++ 529-dosimetrie/scripts/emission_curves.py
@@ -22,8 +22,10 @@ def plot_measurement(measurement):
 print(metadata)
 heating_current_cu, ion_current_cu = plot_measurement("current_1")
 cu_res, (cu_err, cu_rsq) = std.curve_fit(std.linear, heating_current_cu, ion_current_cu)
+std.default.plt_func(std.linear, cu_res, f"$R^2= {round(cu_rsq, 3)}$")
 heating_current_mo, ion_current_mo = plot_measurement("current_2")
 mo_res, (mo_err, mo_rsq) = std.curve_fit(std.linear, heating_current_mo, ion_current_mo)
+std.default.plt_func(std.linear, mo_res, f"$R^2 = {round(mo_rsq, 3)}$")
 std.default.plt_finish("Heizstrom $I_H$ / A", "Ionisationsstrom $I_C$ / A")
 
 
