fc39535543557928d4cb96004b9fb1229bd7f471
Lena <lena@email.com>
2026-06-21 19:32:48 +0200

    tables

--- 529-dosimetrie/scripts/dist_sq.py
+++ 529-dosimetrie/scripts/dist_sq.py
@@ -29,8 +29,8 @@ time = p.ev(data[0], 0.5)
 dose_rate = (dose_after - dose_before) / time
 
 print(r"\hline")
-print(r"Abstand / cm & Dauer / s & $D_\text{1}$ / mSv & $D_\text{2}$ / mSv & Dosisleistung / mSv/s")
-
+print(r"Abstand / cm & Dauer / s & $D_\text{1}$ / mSv & $D_\text{2}$ / mSv & Dosisleistung / mSv/s \\")
+print(r"\hline")
 for i in range(len(dose_rate)):
     print(r"\num{", distance[i].format(), r"} & \num{", time[i].format(), r"} & \num{", dose_before[i].format(), r"} & \num{", dose_after[i].format(), r"} & \num{", dose_rate[i].format(), r"} \\")
 print(r"\hline")
@@ -38,8 +38,8 @@ print(r"\hline")
 
 fit, (errs, rsq) = std.curve_fit(quadratic, distance, dose_rate)
 params = p.ev(fit, errs)
-print("\n parameter a & c:")
-[print(params[i].format()) for i in range(len(fit))]
+#print("\n parameter a & c:")
+#[print(params[i].format()) for i in range(len(fit))]
 
 #plt.scatter(~distance, ~(1/np.sqrt(dose_rate)))
 plt.errorbar(~distance, ~dose_rate, xerr = p.ve(distance)[1], yerr = p.ve(dose_rate)[1], label = "Messwerte", color = "tab:blue", **std.default.error_bar_def)
