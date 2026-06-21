ced7bda8bf7bb278f36cef34cfcaf11e3f17eb8f
Lena <lena@email.com>
2026-06-21 20:41:37 +0200

    jfl

--- 529-dosimetrie/scripts/dist_sq.py
+++ 529-dosimetrie/scripts/dist_sq.py
@@ -11,6 +11,9 @@ fhandle = open("../data/meta.toml", "rb")
 meta = tomllib.load(fhandle)
 fhandle.close()
 
+def convert(x):
+    return x / 32.4
+
 
 
 def quadratic(x, a, c):
@@ -28,18 +31,20 @@ time = p.ev(data[0], 0.5)
 
 dose_rate = (dose_after - dose_before) / time
 
-# print(r"\hline")
-# print(r"Abstand / cm & Dauer / s & $D_\text{1}$ / mSv & $D_\text{2}$ / mSv & Dosisleistung / mSv/s \\")
-# print(r"\hline")
-# for i in range(len(dose_rate)):
-#     print(r"\num{", distance[i].format(), r"} & \num{", time[i].format(), r"} & \num{", dose_before[i].format(), r"} & \num{", dose_after[i].format(), r"} & \num{", dose_rate[i].format(), r"} \\")
-# print(r"\hline")
+equiv_rate = convert(dose_rate)
+
+print(r"\hline")
+print(r"Abstand / cm & Dauer / s & $H_\text{1}$ / mSv & $H_\text{2}$ / mSv & h / mSv/s \\")
+print(r"\hline")
+for i in range(len(dose_rate)):
+    print(r"\num{", distance[i].format(), r"} & \num{", time[i].format(), r"} & \num{", dose_before[i].format(), r"} & \num{", dose_after[i].format(), r"} & \num{", dose_rate[i].format(), r"} \\")
+print(r"\hline")
 
 fit, (errs, rsq) = std.curve_fit(quadratic, distance, dose_rate)
 params = p.ev(fit, errs)
 
-print("parameter a & c:")
-[print(params[i].format()) for i in range(len(fit))]
+#print("parameter a & c:")
+#[print(params[i].format()) for i in range(len(fit))]
 
 #plt.scatter(~distance, ~(1/np.sqrt(dose_rate)))
 plt.errorbar(~distance, ~dose_rate, xerr = p.ve(distance)[1], yerr = p.ve(dose_rate)[1], label = "Messwerte", color = "tab:blue", **std.default.error_bar_def)
