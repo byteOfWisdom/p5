f6392c57cceda49f596478127c183856bcd81a14
Leoni <leoni.dessau@mailbox.org>
2026-06-23 20:02:57 +0200

    current state

--- 529-dosimetrie/scripts/emission_curves.py
+++ 529-dosimetrie/scripts/emission_curves.py
@@ -89,3 +89,33 @@ dose_y = plt.gca().secondary_yaxis("right",functions=(lambda x: 32.4 * x, lambda
 dose_y.set_ylabel("Äquivalentsdosisleistung / $mSvs^{-1}$")
 std.default.plt_finish("Beschleunigungsspannung $U_B$ / V",  "mittlere Ionisationsdosisleistung <j> / $mAkg^{-1}$")
 
+# %%
+heating_current_cu, ion_current_cu, ion_dose_rate_cu = plot_measurement("current_other_1")
+cu_res, (cu_err, cu_rsq) = std.curve_fit(std.linear, heating_current_cu, ion_dose_rate_cu)
+cu_res_ev = p.ev(cu_res, cu_err)
+print("Kupfer fit: a = ", cu_res_ev[0].format(), ", b = ", cu_res_ev[1].format())
+std.default.plt_func(std.linear, cu_res, f"$R^2= {round(cu_rsq, 3)}$")
+heating_current_mo, ion_current_mo, ion_dose_rate_mo = plot_measurement("current_other_2")
+mo_res, (mo_err, mo_rsq) = std.curve_fit(std.linear, heating_current_mo, ion_dose_rate_mo)
+mo_res_ev = p.ev(mo_res, mo_err)
+print("Molybdän fit: a = ", mo_res_ev[0].format(), ", b = ", mo_res_ev[1].format())
+std.default.plt_func(std.linear, mo_res, f"$R^2 = {round(mo_rsq, 3)}$")
+
+dose_y = plt.gca().secondary_yaxis("right",functions=(lambda x: 32.4 * x, lambda x: x / 32.4))
+dose_y.set_ylabel("Äquivalentsdosisleistung / $mSvs^{-1}$")
+std.default.plt_finish("Heizstrom $I_H$ / A", "mittlere Ionisationsdosisleistung <j> / $mAkg^{-1}$")
+
+cu_current_table = {
+    "I_H / mA": heating_current_cu,
+    "I_C / A": ion_current_cu,
+    "$<j>$ / $Akg^{-1}$": ion_dose_rate_cu,
+}
+std.print_tex_table(cu_current_table, "../latex/cu_current_other_device.table")
+
+mo_current_table = {
+    "I_H / mA": heating_current_mo,
+    "I_C / A": ion_current_mo,
+    "$<j>$ / $Akg^{-1}$": ion_dose_rate_mo,
+}
+std.print_tex_table(mo_current_table, "../latex/mo_current_other_device.table")
+
