fixed units in emission lines

529-dosimetrie/scripts/shielding.py
@@ -9,25 +9,36 @@ fhandle.close()
 
 thickness_lut = np.array([0, 0.5, 1., 1.5, 2., 2.5, 3.])
 
+def lin_corr(x):
+    a = p.from_string("0.4882(35)")
+    b = p.from_string("0.01080(22)")
+    return (a * x + b) / (a + b)
+
+
 def current_corrected_countrate(current, countrate):
-    return countrate / current # TODO: is this good enough?
+    return countrate * lin_corr(current)
+    # return countrate / current # TODO: is this good enough?
 
 ccc = current_corrected_countrate
 
-def absorber(x, u):
-    return np.exp(- x * u)
+def absorber(x, u, a):
+    return np.exp(- x * u + a)
+
 
 def process_measurement(params):
     print(params)
     data = std.load_csv("../data/" + params["csv_file"], skiprows=1)
     angle, current, countrate = data[0], data[1], data[2]
+    key = np.argsort(angle)
+    current = current[key]
+    countrate = countrate[key]
     countrate = p.ev(countrate, np.sqrt(countrate))
     actual_rate = ccc(current, countrate)
 
-    thickness = thickness_lut[np.argsort(angle)]
-    transmission = actual_rate / max(actual_rate)
+    thickness = thickness_lut
+    transmission = actual_rate / actual_rate[0]
 
-    res, (err, rsq) = std.curve_fit(absorber, thickness, transmission)
+    res, (err, rsq) = std.curve_fit(absorber, thickness[1:], transmission[1:])
     std.default.plt_errorbar(thickness, transmission, "mit Filter" if params["filter"] else "ohne Filter")
     std.default.plt_func(absorber, res, f"$R^2 = {rsq}$", (0, 3))
 
@@ -35,7 +46,9 @@ def process_measurement(params):
 
 
 
-process_measurement(metadata["messung_1"])
-process_measurement(metadata["messung_2"])
+res_a = process_measurement(metadata["messung_1"])
+res_b = process_measurement(metadata["messung_2"])
+print(res_a)
+print(res_b)
 std.default.plt.yscale("log")
 std.default.plt_finish("Dicke / mm", "Transmissivität / 1")