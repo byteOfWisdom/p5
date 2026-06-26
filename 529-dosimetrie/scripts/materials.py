c02a935214e40faa74976df3c50eb2e5b15a189a
Leoni <leoni.dessau@mailbox.org>
2026-06-26 16:54:29 +0200

    aasdf

--- 529-dosimetrie/scripts/materials.py
+++ 529-dosimetrie/scripts/materials.py
@@ -15,6 +15,7 @@ transmission_b = shielding.load_and_normalize(params_b["csv_file"])
 
 materials = ["Leer", "C", "Al", "Fe", "Cu", "Zr", "Ag"]
 material_atomic_nums = [0, 6, 13, 26, 29, 40, 47]
+literature_coeffs = [0, 0, 0.929, 0, 0, 0, 0]
 
 mu_a = p.ev(0.850, 0.046)
 mu_b = p.ev(0.828, 0.024)
@@ -31,4 +32,5 @@ absorb_coeff_b = - np.log(transmission_b) / thickness_measured_a
 
 std.default.plt_errorbar(material_atomic_nums[1:], absorb_coeff_a[1:], "Ohne Filter")
 std.default.plt_errorbar(material_atomic_nums[1:], absorb_coeff_b[1:], "Mit Filter")
-std.default.plt_finish("Ordnungszahl", "$\\mu$")
+std.default.plt_errorbar(material_atomic_nums[1:], literature_coeffs[1:], "Literatur")
+std.default.plt_finish("Ordnungszahl", "$\\mu$ / $mm^{-1}$")
