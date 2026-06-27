39fc5823f5faf57228cd372c83393302b7dd18e0
Lena <lena@email.com>
2026-06-27 20:03:18 +0200

    more text

--- 529-dosimetrie/scripts/dist_sq.py
+++ 529-dosimetrie/scripts/dist_sq.py
@@ -27,7 +27,7 @@ def get_data(number: str):
     dose_before = p.ev(data[1], 0.1)
     dose_after = p.ev(data[2], 0.1)
     time = p.ev(data[0], 0.5)
-    print(f"i have {len(distance)} points")
+    #print(f"i have {len(distance)} points")
     #print(f"done getting data for {number}")
     return distance, dose_before, dose_after, time, number
 
@@ -36,12 +36,12 @@ def handle_data(distance, dose_before, dose_after, time, number):
 
     equiv_rate = convert(dose_rate)
 
-    # print(r"\hline")
-    # print(r"Abstand / cm & Dauer / s & $H_\text{1}$ / mSv & $H_\text{2}$ / mSv & h / mSv/s \\")
-    # print(r"\hline")
-    # for i in range(len(dose_rate)):
-    #     print(r"\num{", distance[i].format(), r"} & \num{", time[i].format(), r"} & \num{", dose_before[i].format(), r"} & \num{", dose_after[i].format(), r"} & \num{", dose_rate[i].format(), r"} \\")
-    # print(r"\hline")
+    print(r"\hline")
+    print(r"Abstand / cm & Dauer / s & $H_\text{1}$ / mSv & $H_\text{2}$ / mSv & h / mSv/s \\")
+    print(r"\hline")
+    for i in range(len(dose_rate)):
+        print(r"\num{", distance[i].format(), r"} & \num{", time[i].format(), r"} & \num{", dose_before[i].format(), r"} & \num{", dose_after[i].format(), r"} & \num{", dose_rate[i].format(), r"} \\")
+    print(r"\hline")
 
     fit, (errs, rsq) = std.curve_fit(quadratic, distance, dose_rate)
     params = p.ev(fit, errs)
@@ -52,7 +52,7 @@ def handle_data(distance, dose_before, dose_after, time, number):
 
     messung = "Mo" if number == "1" else "Cu"
 
-    print(f"i have {len(dose_rate)} points for {messung}")
+   # print(f"i have {len(dose_rate)} points for {messung}")
     plt.errorbar(~distance, ~dose_rate, xerr = p.ve(distance)[1], yerr = p.ve(dose_rate)[1], label = f"Messwerte {messung}",  **std.default.error_bar_def)
     plt.scatter(~distance, ~dose_rate)
     xrange = np.linspace(min(~distance) - 2, max(~distance) + 2)
@@ -62,12 +62,12 @@ def handle_data(distance, dose_before, dose_after, time, number):
 def main():
     data2 = get_data("2")
     handle_data(*data2)
-    data1 = get_data("1")
-    handle_data(*data1)
+    #data1 = get_data("1")
+    #handle_data(*data1)
     plt.legend()
     std.default.plt_pretty("Abstand / cm", "Dosisleistung / mSv/s")
     #plt.savefig("../figs/dist_sq.pdf")
-    plt.show()
+    #plt.show()
 
 if __name__ == "__main__":
     main()
