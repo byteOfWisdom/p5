3f2315576cb4492d13665db4898c8de8a8cd19ee
Leoni <leoni.dessau@mailbox.org>
2026-07-09 09:52:39 +0200

    adsf

--- 525-nukleare-elektronik/scripts/time_cal.py
+++ 525-nukleare-elektronik/scripts/time_cal.py
@@ -3,9 +3,11 @@ import numpy as np
 import propeller as p
 
 
-def load_data():
+def load_data(sigma=False):
     fname = "../data/fits/prompt.csv"
     data = std.load_csv(fname, skiprows=1)
+    if sigma:
+        return data[2]
     return data[1]
 
 def time_cal(plot=False):
@@ -18,7 +20,8 @@ def time_cal(plot=False):
         std.default.plt_errorbar(lines, times,marker="x")
         std.default.plt_func(std.linear, res, f"$R^2 = {round(rsq, 4)}$")
         # std.default.plt_finish("Zeit / ns", "Linie / Bins")
-        std.default.plt_finish("Linie / Bins", "Zeit / ns")
+        std.default.plt_finish("Linie / Bins", "Zeit / ns", "../figs/time_cal.pdf")
+        # std.default.plt_finish("Linie / Bins", "Zeit / ns")
     return p.ev(res, err)
 
 
@@ -28,4 +31,25 @@ def invert(a, b):
 
 
 if __name__ == "__main__":
-    print(time_cal(True))
+    res = time_cal(True)
+    print(res)
+
+    # std.write_file("../data/fits/time_cal_fit.txt", "$a = \\SI{" + res[0].format() + "}{\\nano\\second\\per\\bin}$ und $b = \\SI{" + res[1].format() + "}{\\nano\\second}$")
+    std.write_file("../data/fits/time_cal_fit.txt", std.si_string("a", res[0], "\\nano\\second\\per\\bin") + " und " + std.si_string("b", res[1], "\\nano\\second"))
+
+    sigmas = load_data(True)
+    fwhm = np.sqrt(8 * np.log(2)) * sigmas
+    time_res = fwhm * res[0]
+    print(fwhm)
+    print(time_res)
+    table = {
+        "\\sigma / Bin": sigmas,
+        "FWHM / Bin": fwhm,
+        "FWHM / ns": time_res
+    }
+
+    std.print_tex_table(table, "../latex/time_resolution.table")
+
+    std.write_file("../data/fits/fwhm_time_res.txt", std.si_string("\\text{FWHM}" ,(sum(time_res) / len(time_res)).format(), "\\nano\\second"))
+    print(sum(time_res) / len(time_res))
+    
